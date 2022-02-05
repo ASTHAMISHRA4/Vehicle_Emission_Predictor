9094652438125e8f8a9c71996be650db253c94c4
Astha Mishra <52097499+ASTHAMISHRA4@users.noreply.github.com>
2022-02-05 14:13:41 -0800

    Initial commit

---
+++ SimpleLinearRegression.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+
+df = pd.read_csv("FuelConsumption.csv")
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+
+# STEP 1 : let's plot each of these features against the Emission, to see how linear their relationship is:
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("CYLINDERS")
+plt.ylabel("Emission")
+plt.show()
+
+#STEP 2 : Creating train and test dataset
+#Let's split our dataset into train and test sets.
+#80% of the entire dataset will be used for training and 20% for testing.
+# We create a mask to select random rows using np.random.rand() function:
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+#STEP 3 : Training model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+# The coefficients
+print('Coefficients: ', regr.coef_)
+print('Intercept: ', regr.intercept_)
+
+#Plotting fit line
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+#STEP 4 : Prediction
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+#STEP 5 : Evaluation
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y, test_y_))
